b200ee8817b6af767b69b58295cb23e54e966fbf
elfarsif <f.n.elfarsi@gmail.com>
2023-11-02 09:26:51 -0400

    sping-app-november maven project added empty

---
+++ backend/spring-app-november/src/main/java/com/spring/springappnovember/SpringAppNovemberApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springappnovember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppNovemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppNovemberApplication.class, args);
+	}
+
+}
---
+++ backend/spring-app-november/src/test/java/com/spring/springappnovember/SpringAppNovemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springappnovember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppNovemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
