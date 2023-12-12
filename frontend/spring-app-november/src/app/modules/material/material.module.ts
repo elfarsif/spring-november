3ccb24c3950df40cb52050cf1adba02724593594
elfarsif <f.n.elfarsi@gmail.com>
2023-12-11 21:13:41 -0500

    Actually new branch is for header features, added the drop down

--- frontend/spring-app-november/src/app/modules/material/material.module.ts
+++ frontend/spring-app-november/src/app/modules/material/material.module.ts
@@ -12,6 +12,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatListModule } from '@angular/material/list';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [],
@@ -29,6 +30,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
     MatGridListModule,
     MatListModule,
     MatExpansionModule,
+    MatMenuModule,
   ],
 })
 export class MaterialModule {}
